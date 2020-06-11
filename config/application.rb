246fd784abe269bae42d66a9974df88e31d8b969
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-06-11 12:35:34 +0000

    ジェネレータをカスタマイズする

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,24 @@ module RailsDocker
     config.active_record.default_timezone = :local
 
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+      g.test_framework :rspec,
+                       # テストデータベースにレコードを作成するファイルの作成をスキップ
+                       fixtures: false,
+                       # ビュースペックの作成をスキップ(UIのテストはフィーチャスペックに任せる)
+                       view_specs: false,
+                       # ヘルパースペックの作成をスキップ
+                       helper_specs: false,
+                       # ルーティングスペックの作成をスキップ
+                       routing_specs: false,
+                       # コントローラースペックの作成をスキップ
+                       controller_specs: false,
+                       # リクエストスペックの作成をスキップ
+                       request_specs: false
+    end
   end
 end
